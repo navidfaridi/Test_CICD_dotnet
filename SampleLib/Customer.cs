044c0cd4ae9aeb9820fb949947291821b7a76c73
Navid Faridi <navid.faridi@a5labs.co>
2022-12-23 15:50:42 +0300

    first

---
+++ SampleLib/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleLib
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+
+}
